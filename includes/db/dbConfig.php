d3173a25da97b929eb8025de41197fbb3cfaaa77
diego <diegocesari22@gmail.com>
2025-11-17 21:02:50 -0300

    Update dbConfig.php

--- includes/db/dbConfig.php
+++ includes/db/dbConfig.php
@@ -2,7 +2,7 @@
     $db_serverName = "localhost";
     $db_username = "root";
     $db_password = "";
-    $db_name = "healthway_bd";
+    $db_name = "healthway";
     $db_strconn = "mysql:host=" . $db_serverName . ";dbname=" . $db_name . ";charset=utf8";
 
     /** @var PDO $conn */
