0378ca4d52da0f44aa3798a050a1ee27dbefd3e3
lucio03-svg <luciogomez03@gmail.com>
2025-11-25 15:12:07 -0300

    1er Cambio
    Modificación de código y Adición de Archivos

--- modules/administrador/gestionadminusuario.php
+++ modules/administrador/gestionadminusuario.php
@@ -4,11 +4,11 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion de Usuarios - HealthWay</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" xintegrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <?php
+        $module = 'administrador';
+        require_once($dirBaseFile . '/includes/html/head.php');
+        echo '<script src="' . $dirBaseUrl . '/script/logicafrontadmin.js"></script>';
+    ?>
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body class="bg-light">
